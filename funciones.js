76a0cbbe1db25c679b0f3f98b0ed57ef6477db12
malkito <malkito93@hotmail.com>
2021-07-27 15:44:44 -0400

    new changes

---
+++ funciones.js
@@ -0,0 +1,11 @@
+var menuopen = document.getElementById('loterias_menu');
+var submenu = document.getElementById('Submenu');
+
+
+function show_menu(){
+    submenu.classList.toggle('show')
+}
+
+
+let hora = Date();
+console.log(hora)
