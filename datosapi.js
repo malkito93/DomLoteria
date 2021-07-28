0122d62fb8170a98f7fd6ef53e496c3d12e9e2f7
malkito <malkito93@hotmail.com>
2021-07-28 07:51:24 -0400

    new updates cards and dates

---
+++ datosapi.js
@@ -0,0 +1,19 @@
+const Api_Url = 'https://jsonplaceholder.typicode.com/posts';
+const bolo1_Nacional = document.getElementById('bolo1_nacional')
+const bolo2_Nacional = document.getElementById('bolo2_nacional')
+const bolo3_Nacional = document.getElementById('bolo3_nacional')
+//consumir id para numeros bolos
+
+fetch (Api_Url)
+.then(res => res.json())
+.then(data => {
+    data.forEach(resultado => {
+        console.log(resultado.userId)
+        var bolos = resultado.userId 
+        bolo1_Nacional.textContent = bolos -8;
+        bolo2_Nacional.textContent = bolos;
+        bolo3_Nacional.textContent = bolos -6;
+    });
+    
+})
+.catch (err => console.log(err))
--- funciones.js
+++ funciones.js
@@ -1,11 +1,41 @@
 var menuopen = document.getElementById('loterias_menu');
 var submenu = document.getElementById('Submenu');
+var fechaActual = document.getElementById('cards_dates')
+var fechaActualNacional = document.getElementById('cards_dates_nacional')
 
 
+//ESTO ES UNA PRUEBA
+
+//HASTA AQUI ES PRUEBA 
+
+var fechaMananaLeidsa = document.getElementById('cards_tomrow_leidsa') 
+var fechaMananaNacional = document.getElementById('cards_tomrow_nacional')
+
+
+// Menu plegable (Submenu)
 function show_menu(){
     submenu.classList.toggle('show')
 }
 
+//OPERACION FECHA DE HOY 
+let hoy = new Date()
+const meses = ['enero', 'Febrero','Marzo', 'Abril', 'Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre', 'Disciembre']
 
-let hora = Date();
-console.log(hora)
+let dia = hoy.getDate();
+let mes = hoy.getMonth();
+let anio = hoy.getFullYear();
+
+let pmes = meses[mes]
+//AA-MM-DD
+let formato1 = `${dia} de ${pmes} (${anio})`
+let diaDeManana = `${dia + 1} de ${pmes} (${anio})`
+
+
+// FECHA DE HOY CARDS
+fechaActual.textContent = formato1;
+fechaActualNacional.textContent = formato1;
+
+// FECHA DE MANANA CARDS
+fechaMananaLeidsa.textContent = diaDeManana;
+fechaMananaNacional.textContent = diaDeManana;
+
