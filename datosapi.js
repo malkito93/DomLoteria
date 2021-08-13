496405a09702856e9f157594c81907a9c3affc84
malkito <malkito93@hotmail.com>
2021-08-13 10:23:56 -0400

    new changes

--- datosapi.js
+++ datosapi.js
@@ -1,19 +1,20 @@
-const Api_Url = 'https://jsonplaceholder.typicode.com/posts';
+// const Api_Url = 'https://jsonplaceholder.typicode.com/posts';
+const url = 'http://173.248.151.142:8025/DomLoteria/api/';
 const bolo1_Nacional = document.getElementById('bolo1_nacional')
 const bolo2_Nacional = document.getElementById('bolo2_nacional')
 const bolo3_Nacional = document.getElementById('bolo3_nacional')
 //consumir id para numeros bolos
 
-fetch (Api_Url)
-.then(res => res.json())
-.then(data => {
-    data.forEach(resultado => {
-        // console.log(resultado.userId)
-        var bolos = resultado.userId 
-        bolo1_Nacional.textContent = bolos -8;
-        bolo2_Nacional.textContent = bolos;
-        bolo3_Nacional.textContent = bolos -6;
-    });
+fetch (url)
+.then(res => console.log(res))
+// .then(data => {
+//     data.forEach(resultado => {
+//         // console.log(resultado.userId)
+//         // var bolos = resultado.userId 
+//         // bolo1_Nacional.textContent = bolos -8;
+//         // bolo2_Nacional.textContent = bolos;
+//         // bolo3_Nacional.textContent = bolos -6;
+//     });
     
-})
-.catch (err => console.log(err))
+// })
+// .catch (err => console.log(err))
